c70fae8663116bf79e647f3a3d47814ac9a0c506
gouravcod <gourava.kini@tibilsolutions.com>
2025-04-25 09:57:07 +0530

    added delete and authguard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,10 +6,14 @@ import {
     Post,
     Body,
     ParseIntPipe,
+    Query,
+    Delete,
+    UseGuards,
   } from '@nestjs/common';
   import { UsersService } from './users.service';
   import { CreateUserDto } from './dto/create-user.dto';
   import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from './jwt-auth.guard';
   
   @Controller('dynamic-form')
   export class UsersController {
@@ -21,28 +25,52 @@ import {
       return this.usersService.create(dto);
     }
 
-    @Get('getAllusers')
-    findAll() {
-      return this.usersService.findAll();
-    }
+   
   
     @Get('getUser/:id')
     findOne(@Param('id', ParseIntPipe) id: number) {
       return this.usersService.findOne(id);
     }
-  
-   
+
+    @Get('getUserById')
+    getMany(@Query('ids') ids: string) {
+      const idArray = ids
+    .split(',')
+    .map(id => parseInt(id))
+    
+  const users_array = this.usersService.findManyByIds(idArray);
+
+  return {
+    message: 'Users fetched successfully',
+    data: users_array,
+  };
+}
   
     @Put('updateUser/:id')
     update(@Param('id', ParseIntPipe) id: number, @Body() updateUserDto: UpdateUserDto) {
       return this.usersService.update(id, updateUserDto);
     }
+    
+
+    @Delete('deleteUser/:id')
+    delete(@Param('id', ParseIntPipe) id: number) {
+      const user = this.usersService.findOne(id);
+      const index = this.usersService.findAll().indexOf(user);
+      this.usersService.findAll().splice(index, 1);
+      return {
+        message: 'User deleted successfully',
+        user,
+      };
+    }
 
 
+    @UseGuards(JwtAuthGuard)
+    @Get("getAllTasks")
+    gettasks(){
+      return this.usersService.findAll();
+    }
 
-    validateEmail(email: string): boolean {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailRegex.test(email);
-      }
+
+    
   }
   
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,32 +1,41 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UsersService {
   private users: any[] = [];
 
     private nextId = 1;
+    constructor(private readonly jwtService: JwtService) {}
 
     create(createUserDto: CreateUserDto) {
-        const phoneExists = this.users.some(user => user.phone === createUserDto.phone);
-        if (phoneExists) {
-          throw new BadRequestException('Phone number already exists');
-        }
-      
-        const newUser = {
-          id: this.nextId++,
-          ...createUserDto,
-        };
-      
-        this.users.push(newUser);
-        return {
-          message: 'User created successfully',
-          user: newUser,
-        };
+      const phoneExists = this.users.some(user => user.phone === createUserDto.phone);
+      if (phoneExists) {
+        throw new BadRequestException('Phone number already exists');
       }
+    
+      const newUser = {
+        id: this.nextId++,
+        ...createUserDto,
+      };
+    
+      this.users.push(newUser);
+    
+   
+      const payload = { id: newUser.id, phone: newUser.phone, name: newUser.name };
+      const token = this.jwtService.sign(payload);
+    
+      return {
+        message: 'User created successfully',
+        user: newUser,
+        accessToken: token,
+      };
+    }
+    
       
-
+  
   findAll() {
     return this.users;
   }
@@ -36,6 +45,16 @@ export class UsersService {
     if (!user) throw new Error('User not found');
     return user;
   }
+
+  findManyByIds(ids: number[]) {
+    const foundUser = this.users.filter(user => ids.includes(user.id));
+
+    if (foundUser.length === 0) {
+        throw new NotFoundException('No users found for the provided IDs');
+      }
+      return foundUser;
+
+}   
 
   update(id: number, updateUserDto: UpdateUserDto) {
     const user = this.findOne(id);
